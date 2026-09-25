7b958da49aea8b55ec3b931bda7f9b79098eea22
Alexis JoseG Sierra <Alexisjose155568@gmail.com>
2026-09-25 15:52:16 -0400

    Auto-generated commit

--- chunk-SLEROSNW.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2VMXMS7J.js";var t=[{path:"",loadComponent:()=>import("./chunk-WDDP4SCP.js").then(o=>o.CatalogPageComponent)}];export{t as CATALOG_ROUTES};
---
+++ chunk-XZBWH7QJ.js
@@ -0,0 +1,1 @@
+import"./chunk-FDERIQAA.js";var t=[{path:"",loadComponent:()=>import("./chunk-HDBQCCME.js").then(o=>o.CatalogPageComponent)}];export{t as CATALOG_ROUTES};
